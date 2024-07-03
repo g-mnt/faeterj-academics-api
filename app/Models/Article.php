567c118bd3c72b42bfc8dcb087aad075e90dcdd4
Gabriel Monteiro <gmonteiro.arg@gmail.com>
2024-07-02 21:41:13 -0300

    wip

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Storage;
 
 class Article extends Model
 {
@@ -21,5 +23,11 @@ class Article extends Model
 
     public function author(): BelongsTo {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    protected function documentUrl(): Attribute {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => Storage::temporaryUrl($attributes['document_path'], now()->addDay())
+        )->shouldCache();
     }
 }
